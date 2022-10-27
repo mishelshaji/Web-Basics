63f4b7d98a2cf7a1da54dcd656478764cf3bffdc
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-10-27 09:53:30 +0530

    Basic tag properties

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+var tag = document.getElementById('main');
+tag.innerText = "New Text";
+tag.style.color = "red";
+tag.style.fontSize = '50px';
+tag.style.fontWeight = 'normal';
+// console.log(tag.innerText);
