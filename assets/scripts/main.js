1e34a4fb90a32cecbed61816137add7973ee9d61
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-10-27 09:57:52 +0530

    Getting parent element

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,4 @@
 var tag = document.getElementById('main');
-tag.innerText = "New Text";
-tag.style.color = "red";
-tag.style.fontSize = '50px';
-tag.style.fontWeight = 'normal';
+console.log(tag.parentElement);
+tag.parentElement.style.backgroundColor = 'teal';
 // console.log(tag.innerText);
