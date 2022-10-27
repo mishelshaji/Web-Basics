f0959cf66439e2f444d1646dcdf61fe82a001452
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-10-27 11:15:54 +0530

    Creating html elements

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,11 @@
-var qs = document.querySelectorAll('div>button');
+var table = document.createElement('table');
 
-qs.forEach(e=>{
-    e.style.backgroundColor = 'tomato';
-})
+var tr = document.createElement('tr');
+var th1 = document.createElement('th')
+var th2 = document.createElement('th')
+th1.innerText = "Name";
+th2.innerText = "Email";
+tr.appendChild(th1);
+tr.appendChild(th2);
+table.appendChild(tr);
+document.body.appendChild(table);
