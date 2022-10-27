3d7a5541b7ed7b3a5174c523af4fec902bacd1d9
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-10-27 12:58:39 +0530

    Simple form validation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('button').forEach(e=>{
-    e.classList.add('bg-primary');
-    e.classList.remove('text-white');
-})
-
-// function btnClicked() { 
-//     console.log("Button clicked");
-// }
+function validate(e) {  
+    e.preventDefault();
+    var nameValue = document.forms['loginform']['name'].value;
+    if(nameValue.length <= 2) {
+        document.getElementById('name').style.borderColor = 'red';
+        document.getElementById('error-name').innerText = "Invalid Name";
+    }
+}
