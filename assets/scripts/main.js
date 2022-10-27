325919c63e1f5ca744a6375b2eda606143fcfe45
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-10-27 11:26:24 +0530

    Handling events

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,7 @@
-var table = document.createElement('table');
+function btnClick(){
+    alert("Button clicked");
+}
 
-var tr = document.createElement('tr');
-var th1 = document.createElement('th')
-var th2 = document.createElement('th')
-th1.innerText = "Name";
-th2.innerText = "Email";
-tr.appendChild(th1);
-tr.appendChild(th2);
-table.appendChild(tr);
-document.body.appendChild(table);
+function btnMouseOver(){
+    alert("Mouse over the button");
+}
