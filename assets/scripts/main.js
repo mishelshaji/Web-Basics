72d92726a5370220d557c0597b1d0ddcf90062f8
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-10-28 11:40:38 +0530

    Form validation

--- assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function validate(e) {  
-    e.preventDefault();
-    var nameValue = document.forms['loginform']['name'].value;
-    if(nameValue.length <= 2) {
-        document.getElementById('name').style.borderColor = 'red';
-        document.getElementById('error-name').innerText = "Invalid Name";
-    }
-}
